cfd69ee3c5111f824e3d393597c69b442d0e643a
Artsiom Varabei <35666863+captainhorsepower@users.noreply.github.com>
2021-03-18 00:40:34 +0300

    init flutter project

---
+++ six_dof_camera_app/README.md
@@ -0,0 +1,5 @@
+# 6DoF camera app
+
+A flutter project.
+
+Captures video from mobile device camera and enhances it with 6DoF data from ARKit.
---
+++ six_dof_camera_app/android/app/src/main/kotlin/edu/bsu/famcs/artemvrby/six_dof_camera_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.bsu.famcs.artemvrby.six_dof_camera_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
