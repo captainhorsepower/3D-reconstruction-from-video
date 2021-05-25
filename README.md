581809e8cffff3a827b729ccd1095be2bcce04ca
Artsiom Varabei <35666863+captainhorsepower@users.noreply.github.com>
2021-05-25 16:48:53 +0300

    dump links, and done for now

--- README.md
+++ README.md
@@ -57,3 +57,31 @@ Output
 - opencv [1](https://stackoverflow.com/questions/18052337/how-to-verify-that-the-camera-calibration-is-correct-or-how-to-estimate-the-er), [2](https://stackoverflow.com/questions/33068051/how-alignregister-and-merge-point-clouds-to-get-full-3d-model), [3](https://docs.opencv.org/master/d3/d14/tutorial_ximgproc_disparity_filtering.html)
 
 - [stereo matching algs](https://vision.middlebury.edu/stereo/taxonomy-IJCV.pdf), [SGBM tuner](https://github.com/SebastinSanty/SGBMTuner), [SGM paper](https://core.ac.uk/download/pdf/11134866.pdf)
+
+```
+В компьютерном зрении диспаратность часто рассматривается как синоним обратной глубины. Совсем недавно несколько исследователей определили диспаратность как трехмерное проекционное преобразование (коллинеацию или гомографию) трехмерного пространства (X, Y, Z). Перечисление всех возможных совпадений в таком обобщенном пространстве диспаратности может быть легко достигнуто с помощью алгоритма развертки плоскости [30, 113], который для каждого значения диспаратности d проецирует все изображения на общую плоскость с использованием перспективной проекции (гомографии).
+```
+
+- https://blog.markdaws.net/arkit-by-example-part-4-realism-lighting-pbr-b9a0bedb013e
+
+- https://www.researchgate.net/post/How_to_properly_shift_one_view_to_match_the_other_with_known_disparity
+
+- https://stackoverflow.com/questions/63661474/how-can-i-encode-an-array-of-simd-float4x4-elements-in-swift-convert-simd-float
+
+- https://stackoverflow.com/questions/25452402/opencv-projection-matrices-from-fundamental-matrix-and-solvepnpransac-are-total
+
+- https://stackoverflow.com/questions/45437037/arkit-what-do-the-different-columns-in-transform-matrix-represent
+
+- https://medium.com/macoclock/augmented-reality-911-transform-matrix-4x4-af91a9718246
+
+- https://stackoverflow.com/questions/47536580/get-camera-field-of-view-in-ios-11-arkit
+
+- https://www.digitalbridge.com/blog/can-we-use-stereo-vision-with-arkit-to-estimate-floor-plans
+
+- https://stackoverflow.com/questions/23275877/opencv-get-perspective-matrix-from-translation-rotation
+
+- https://math.stackexchange.com/questions/336/why-are-3d-transformation-matrices-4-times-4-instead-of-3-times-3
+- https://blender.stackexchange.com/questions/197447/what-is-the-meaning-of-4x4-projection-matrix
+
+- https://cmsc426.github.io/sfm/
+
--- presentation.py
+++ presentation.py
@@ -34,9 +34,71 @@ def computeRectification(F, img0, img1):
     # may need to swam img0, img1 = img1, img0
     return pr, img0, img1
 
+def computeDisparities(imgL, imgR):
+    # disparity range is tuned for 'aloe' image pair
+    window_size = 10
+    min_disp = 16
+    num_disp = 16*20-min_disp
+    stereo = cv.StereoSGBM_create(
+        minDisparity = min_disp,
+        numDisparities = num_disp,
+        blockSize = 15,
+        P1 = 8*3*window_size**2,
+        P2 = 32*3*window_size**2,
+        disp12MaxDiff = -1,
+        uniquenessRatio = 10,
+        speckleWindowSize = 150,
+        speckleRange = 2
+    )
+    
+
+    print('computing disparity...')
+    return stereo.compute(imgL, imgR).astype(np.float32) / 16.0, min_disp, num_disp
+
+def write_ply(fn, verts, colors):
+    ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
+def generate_point_cloud(img0, disp):
+    size = np.array(img0.shape[1::-1]) // 2
+    img0 = cv.resize(img0, size)
+    disp = cv.resize(disp, size)
+
+    print('generating 3d point cloud...',)
+    h, w = img0.shape[:2]
+    f = 0.8*w                          # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0,     -f], # so that y-axis looks up
+                    [0, 0, 1,      0]])
+    points = cv.reprojectImageTo3D(disp, Q)
+    colors = cv.cvtColor(img0, cv.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+    out_fn = 'out.ply'
+    write_ply(out_fn, out_points, out_colors)
+    print('%s saved' % out_fn)
+
+
 def main():
     recording = 'test-data/forward'
-    frames = (10, 20)
+    frames = (8, 13)
     
     img0, img1 = [cv.imread(f'{recording}/{f}.png') for f in frames]
 
@@ -51,8 +113,20 @@ def main():
     polar0 = remap(img0, pr.forward_map(0))
     polar1 = remap(img1, pr.forward_map(1))
 
-    cv.imshow('polar0', polar0)
-    cv.imshow('polar1', polar1)
+    disp, min_disp, num_disp = computeDisparities(polar0, polar1)
+    polar_disp = disp
+    disp = remap(disp, pr.get_reverse_map(0))
+
+    cv.imshow('polar_disp', (polar_disp-min_disp)/num_disp)
+    cv.imshow('disparity', (disp-min_disp)/num_disp)
+    cv.imshow('left', img0)
+    print('have a look')
+
+    # cv.imwrite('disp/polar.png', polar0)
+    # cv.imwrite('disp/polar_disp.png', polar_disp)
+    # cv.imwrite('disp/left.png', img0)
+    # cv.imwrite('disp/left_disp.png', disp)
+
     cv.waitKey()
     cv.destroyAllWindows()
 
